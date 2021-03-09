25b86babb95d1e6881f3da1307d1743e596ed24c
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-08 22:20:22 -0700

    final changes

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,49 @@
-var time=moment().format("dddd, MMMM Do YYYY, h:mm a");
-console.log(typeof time)
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+//use moment.js to get the time to display on my webpage
+var time = moment();
+var timer=moment().format("dddd, MMMM Do YYYY, h:mm a");
+console.log(typeof timer)
 var currentDay=document.querySelector("#currentDay");
-currentDay.textContent=time
+currentDay.textContent=timer
 
 console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+function setPlanner() {
 
+  //function to save text input into localStorage  
+    $(".contentBlock").each(function () {
+        var id = $(this).attr("id");
+        var dailyActivity = localStorage.getItem(id);
 
-
-for(let hour = 8; hour<=17; hour++){
-    let index=hour-8;
+        if (dailyActivity !== null) {
+            $(this).children(".dailyActivity").val(dailyActivity);
+        }
+    });
 }
 
-let lineBlock = $('<div>');
-lineBlock.addClass('rowPlanner');
-lineBlock.addClass('index');
+setPlanner();
+var saveBtn = $(".saveBtn");
+//create event to save info onto localStorage once btn is clicked
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var dailyActivity = $(this).siblings(".dailyActivity").val();
 
-let saveBtn = $('<i>');
-    saveBtn.attr('id',`saveid-[0]`);
-    saveBtn.attr('save-id',[0]);
-    saveBtn.attr('class',"fas fa-save saveIcon");
-
-
-    
-
-
-
-
-
-//asking browser to go to localStorage and retrieve the data label as planList
-var planList= JSON.parse(localStorage.getItem("planList"));
-console.log(planList);
-
-$(document).on('click','i', function(event) {
-    event.preventDefault();  
-
-})
-
-
-if(planList !==null){
-    planTextArray= planList;
-}
-
-
-
-
+    localStorage.setItem(time, dailyActivity);
+});
+//fucntion to color code time frames as to past, presnet, and future
+function timeColor (){
+    var future=newTime(hour[i++]);
+    var past =oldTime(hour[i--]);
+    for (let i=0; i<hour.length; i++)
+    {if(future>currentDay){document.getElementsByClassName("hourBox").style.backgroundColor="gray";
+}else{document.getElementsByClassName("hourBox").style.backgroundColor="red";
+}return currentDay.getTime()<=future.getTime();}}
 
 
